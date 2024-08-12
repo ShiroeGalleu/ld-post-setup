b2c83b39e91ed02da17326e30e9ef3239f1a5c09
Shiroe Galleu <alexanderallen8719@gmail.com>
2024-08-11 23:50:40 -0500

    Update ldps.sh

--- ldps.sh
+++ ldps.sh
@@ -1,7 +1,7 @@
 #Shimmy Shitty Post install script for cadmium
 #Version 081124.1
 
-apt install -y sudo libwacom-bin maliit-keyboard maliit-framework kde-plasma-desktop
+apt install -y sudo libwacom-bin maliit-keyboard maliit-framework
 #Fix the Stylus and screen rotate "Thank you Azull"
 mkdir /etc/libwacom/
 cp google-krane.tablet /etc/libwacom/google-krane.tablet
@@ -20,3 +20,6 @@ cd auto-cpufreq && ./auto-cpufreq-installer
 mkdir /etc/sddm.conf.d/
 echo "[General]
 DisplayServer=wayland" > /etc/sddm.conf.d/force_x11.conf
+
+echo "please pick your desktop. Plasma works the best"
+sleep 2
